de635747b49159a510e6f732adf2d554377d25d3
Alec Wolf <alvwolf@ucsc.edu>
2020-05-22 00:41:25 -0700

    walkable area simplified, added exit detecting for when there is a valid path between kid and exit (warning, will not work if both sides of exit are blocked by objects but kid is in center)

--- src/prefabs/Kid.js
+++ src/prefabs/Kid.js
@@ -7,9 +7,9 @@ class Kid extends Phaser.Physics.Arcade.Sprite {
 
         this.params = {
             walkAreaLBound: undefined,  // left (min) y value of walkable area
-            walkAreaRBound: undefined, // right (max) y value of walkable area
-            direction: "right",            // direction kid is walking in
-            isMoving: false,               // whether kid is currently walking or standing still
+            walkAreaRBound: undefined,  // right (max) y value of walkable area
+            direction: "right",         // direction kid is walking in
+            isMoving: false,            // whether kid is currently walking or standing still
         }
 
         // cet center
@@ -18,7 +18,7 @@ class Kid extends Phaser.Physics.Arcade.Sprite {
         // add walkable area debug rectangle
         this.walkAreaRect = this.scene.add.rectangle(this.scene,0,0,0,0,0xFACADE);
         this.walkAreaRect.alpha = 0.5;
-        this.walkAreaRect.setOrigin(0,0.5)
+        this.walkAreaRect.setOrigin(0, 0.5);
         this.walkAreaRect.setDepth(3);
         
         // add to scene and physics
@@ -33,10 +33,8 @@ class Kid extends Phaser.Physics.Arcade.Sprite {
     update() {
         // calculate walkable area
         let currLevel = game.levelParams.renderedLevels[game.levelParams.currLevelIndex];
-        this.params.walkAreaLBound = game.config.width / 2 - currLevel.params.center.x 
-                                     + currLevel.params.borderWidth; // left wall of level
-        this.params.walkAreaRBound = this.params.walkAreaLBound + currLevel.background.width 
-                                     - currLevel.params.borderWidth * 2; // right wall of level
+        this.params.walkAreaLBound = currLevel.params.x0 + currLevel.params.borderWidth; // left wall of level
+        this.params.walkAreaRBound = this.params.walkAreaLBound + currLevel.background.width - 2 * currLevel.params.borderWidth; // right wall of level
         Phaser.Actions.Call(currLevel.moveGroup, (obj) => {
             if(obj.y + obj.scale * obj.height / 2 >= this.y - this.height / 2) { // on the correct y plane
                 if(obj.x > this.params.walkAreaLBound && obj.x < this.x) { // closer than current left bound
@@ -48,9 +46,27 @@ class Kid extends Phaser.Physics.Arcade.Sprite {
             }
         });
 
+        // add safety buffer to walkable area
+        this.params.walkAreaLBound += 5;
+        this.params.walkAreaRBound -= 5;
+
         // draw debug walkable area rectangle
         this.walkAreaRect.setPosition(this.params.walkAreaLBound, this.y - this.height / 2);
         this.walkAreaRect.setSize((this.params.walkAreaRBound - this.params.walkAreaLBound), this.height);
+
+        // test for exit condition
+        if(!game.levelParams.changingLevel) {
+            let exitLeft = currLevel.params.x0 + (currLevel.params.exit.x - currLevel.params.exit.width / 2);
+            let exitRight = currLevel.params.x0 + (currLevel.params.exit.x + currLevel.params.exit.width / 2);
+            if(exitLeft >= this.params.walkAreaLBound && exitLeft <= this.params.walkAreaRBound && 
+               this.params.walkAreaRBound -exitLeft > this.width) {
+                console.log("exit path");
+            }
+            else if(exitRight <= this.params.walkAreaRBound && exitRight >= this.params.walkAreaLBound &&
+                    exitRight - this.params.walkAreaLBound > this.width) {
+                console.log("exit path");
+            }
+        }
     }
 
     // TODO: variable for scare level with setter/getter/modifiers.
