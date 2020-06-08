98bd85bfc0d241040c6a73888ca38ddc2977a93f
danielliao22 <43585541+danielliao22@users.noreply.github.com>
2020-06-07 22:13:56 -0700

    Moved play preload to loading screen

--- src/scenes/Loading.js
+++ src/scenes/Loading.js
@@ -33,6 +33,90 @@ class Loading extends Phaser.Scene {
 
 		this.load.on('progress', this.updateBar, {newGraphics:this.newGraphics,loadingText:loadingText});
 		this.load.on('complete', this.complete, {scene:this.scene});
+
+		//==ASSETS==
+		// Load level data
+        this.load.json('lvl1','./src/levels/lvl1.json');
+        this.load.json('lvl2','./src/levels/lvl2.json');
+        this.load.json('lvl3','./src/levels/lvl3.json');
+
+        // Load level backgrounds
+        this.load.image('lvl1Background', './assets/textures/level1BG.png');
+        this.load.image('lvl2Background', './assets/textures/level2BG.png');
+        this.load.image('atticBackground', './assets/textures/atticBG.png');
+
+        // Load level asset images
+        this.load.image('radio', './assets/textures/radio.png');
+        this.load.image('radioHover', './assets/textures/radioHover.png');
+        
+        this.load.image('clock', './assets/textures/clock.png');
+        this.load.image('clockHover', './assets/textures/clockHover.png');
+        
+        this.load.image('light', './assets/textures/light_OFF.png');
+        this.load.image('lightHover', './assets/textures/light_OFFHover.png');
+        this.load.atlas('anims_light', './assets/textures/anims_light.png', './assets/textures/anims_light.json');
+        this.load.json('lightCollision', './assets/textures/lights_collision.json');
+        
+        this.load.image('tub', './assets/textures/tub.png');
+        this.load.image('tubHover', './assets/textures/tubHover.png');
+        this.load.json('tubCollision', './assets/textures/tub.xml.json');
+        
+        this.load.image('box', './assets/textures/box.png');
+        this.load.image('boxHover', './assets/textures/boxHover.png');
+        
+        this.load.image('cabinet', './assets/textures/cabinet.png');
+        this.load.image('cabinetHover', './assets/textures/cabinetHover.png');
+        
+        this.load.image('lamp', './assets/textures/lamp.png');
+        this.load.image('lampHover', './assets/textures/lampHover.png');
+        this.load.json('lampCollision', './assets/textures/lamp_collision.json');
+        
+        
+        this.load.image('mirror', './assets/textures/mirror.png');
+        this.load.image('mirrorHover', './assets/textures/mirrorHover.png');
+        
+        this.load.image('shelf', './assets/textures/shelf.png');
+        this.load.image('shelfHover', './assets/textures/shelf.png');
+        
+        this.load.image('balloon', './assets/textures/balloon.png');
+        this.load.image('balloonHover', './assets/textures/balloonHover.png');
+        this.load.json('balloonCollision', './assets/textures/balloon_collision.json');
+        
+
+        // UI
+        this.load.image('barBorder', './assets/textures/barBorder.png');
+        this.load.image('barBackFill', './assets/textures/barBackFill.png');
+        this.load.image('resizeToggle', './assets/textures/resizeToggleB.png');
+        this.load.image('moveToggle', './assets/textures/moveToggleB.png');
+
+        // pause menu assets
+        this.load.image('menuButton', './assets/textures/menuButton.png');
+        this.load.image('menuButtonHover', './assets/textures/menuButtonHover.png');
+        this.load.image('restartButton', './assets/textures/restartButton.png');
+        this.load.image('restartButtonHover', './assets/textures/restartButtonHover.png');
+        this.load.image('resumeButton', './assets/textures/resumeButton.png');
+        this.load.image('resumeButtonHover', './assets/textures/resumeButtonHover.png');
+
+        // load characters' images
+        this.load.image('ghost', './assets/textures/ghost.png');
+
+        this.load.image('kid', './assets/textures/kid.png');
+        this.load.json('kidCollision', './assets/textures/kid_collision.json');
+        this.load.atlas('anims_kid', './assets/textures/anims_kid.png', './assets/textures/anims_kid.json');
+        this.load.image('scaredEmote', './assets/textures/scaredEmote.png');
+
+        // load bgm
+        this.load.audio('bgmLoop', './assets/audio/PaltergeistBGMLoop.wav');
+
+        //sfx
+        this.load.audio('chime', './assets/audio/ClockChime.wav');
+        this.load.audio('radioBite', './assets/audio/Radio.wav');
+        this.load.audio('lightSwitch', './assets/audio/LightSwitch.wav');
+        this.load.audio('lightSwitch', './assets/audio/LightSwitch.wav');
+
+        this.load.audio('possession', './assets/audio/PossessionSFX.wav');
+        this.load.audio('unpossession', './assets/audio/UnpossessionSFX.wav');
+        this.load.audio('toggle', './assets/audio/Toggle.wav');
 	}
 	
 	create(){
